3bdad3e91972bd4aa01ceebcef517c4fd0373c5a
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-02-12 23:43:21 +0200

    app.routes updates and created the register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-    { path: '', component: LandingPage}
+    { path: '', component: LandingPage},
+    { path: 'login', component:Login}
 ];
