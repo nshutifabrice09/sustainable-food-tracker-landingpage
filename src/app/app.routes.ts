70060200d5c3178bb4e54abadf4376e7799fa9cf
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-02-12 23:53:50 +0200

    created hero component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { Auth } from '../service/auth';
 
 export const routes: Routes = [
     { path: '', component: LandingPage},
